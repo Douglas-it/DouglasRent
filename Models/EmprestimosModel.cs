b72adcf6fae055401c6e647faa1f26f4266d1d2f
DouglasTech <maicon.santos.eng@outlook.com>
2025-03-21 10:33:41 +0000

    Begin the Project

---
+++ Models/EmprestimosModel.cs
@@ -0,0 +1,16 @@
+﻿namespace DouglasRent.Models
+{
+    public class EmprestimosModel
+    {
+        //Campos da tabela emprestimos no banco de dados
+        public int id { get; set; }
+        public string Recebedor { get; set; }
+
+        public string Fornecedor  { get; set; }
+
+        public string LivroEmprestado { get; set; }
+
+        public Datetime DataEmprestimo { get; set; } = DateTime.Now;
+
+    }
+}
